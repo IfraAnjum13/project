843699a9bb03ae44ef844fdcc3d1a43e3875c24f
ifra <ifraanjum1320@gmail.com>
2024-05-21 04:46:08 +0500

    changes

---
+++ lib/data.dart
@@ -0,0 +1,83 @@
+  // final List<WearItem> WearItems = [
+  //   WearItem(
+  //     imageUrl:
+  //     'https://assets.ajio.com/medias/sys_master/root/20211208/94XG/61afb75cf997ddf8f1427d75/-473Wx593H-462174152-blue-MODEL3.jpg',
+  //     name: 'Blue Top',
+  //     price: 50,
+  //     rating: 4.5,
+  //     description:
+  //     'Elevate your style with this chic blue top. Perfect for casual outings or parties.',
+  //   ),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/20676812/2022/11/24/85ab4d4d-4ae2-412e-93ab-7cf8100c1c891669292373670-Style-Quotient-Women-Navy-Blue-Solid-Smart-Formal-Shirt-5701-1.jpg',
+  //       name: 'Style Quotient Women Smart Shirt',
+  //       price: 60,
+  //       rating: 4.0,
+  //       description:
+  //       'Look smart and trendy with this stylish shirt. Perfect for formal or casual occasions.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://merokeety.com/cdn/shop/files/MEROKEETYPlaidEmpireWaistMaxiDress_7_1400x.jpg?v=1703586710',
+  //       name: 'Waist Maxi Dress',
+  //       price: 70,
+  //       rating: 3.8,
+  //       description:
+  //       'Effortlessly chic, the waist maxi dress flows with grace and style.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://i.pinimg.com/736x/b3/ac/df/b3acdf38011fec51f48b12e1d5dfc682.jpg',
+  //       name: 'Traditional and Contemporary Clothing',
+  //       price: 80,
+  //       rating: 4.3,
+  //       description:
+  //       'From timeless classics to avant-garde creations, traditional and contemporary clothing celebrate the rich tapestry of style through the ages.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://i.pinimg.com/736x/a0/ac/f0/a0acf0ac241cdc829944fdacf1a9d3a7.jpg',
+  //       name: 'Pakistani Traditional Party Wear',
+  //       price: 90,
+  //       rating: 3.5,
+  //       description:
+  //       'Elegance meets tradition in Pakistani party wear, where intricate craftsmanship weaves stories of cultural heritage.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://images.cbazaar.com/images/lavender-blended-cotton-printed-straight-pant-suit-slsacy1689-pl.jpg',
+  //       name: 'Printed Shalwar Kameez',
+  //       price: 100,
+  //       rating: 3.0,
+  //       description:
+  //       'Vibrant prints adorn the classic shalwar kameez, adding a dash of color and charm to traditional Pakistani attire.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://i.pinimg.com/736x/ed/5b/a8/ed5ba83809858803bb17c413de47a50c.jpg',
+  //       name: 'Short Frock',
+  //       price: 110,
+  //       rating: 4.5,
+  //       description:
+  //       'Short frocks redefine elegance with their playful length and timeless sophistication.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://i.pinimg.com/736x/94/23/fb/9423fbf27d1f9591670b00e62d9d8b50.jpg',
+  //       name: 'Frock in Plum Shade',
+  //       price: 120,
+  //       rating: 5.0,
+  //       description:
+  //       'Exude effortless glamour in a plum-hued frock, blending sophistication with rich, regal allure.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://i.pinimg.com/736x/db/4a/15/db4a153069d0d9138295d7a6501fe60e.jpg',
+  //       name: 'Wedding Function Dress',
+  //       price: 130,
+  //       rating: 4.8,
+  //       description:
+  //       'Dazzle in your wedding function dress, a masterpiece of elegance and celebration.'),
+  //   WearItem(
+  //       imageUrl:
+  //       'https://goldenzari.com.pk/cdn/shop/products/BQR_9480-min-scaled_1.jpg?v=1681730185',
+  //       name: 'Wedding Dress',
+  //       price: 140,
+  //       rating: 4.2,
+  //       description:
+  //       'A wedding dress: where dreams meet exquisite craftsmanship, crafting memories to last a lifetime.'),
+  // ];
